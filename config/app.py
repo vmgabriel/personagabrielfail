1a4047b39999d4dbb0f56b781504ba778080c4a1
Gabriel <vmgabriel96@gmail.com>
2022-05-10 16:38:38 -0500

    Feat: First Commit With All

---
+++ config/app.py
@@ -0,0 +1,5 @@
+"""App Settings"""
+
+MDA_APPS = [
+    "personagabrielfail.apps.personagabrielfailConfig",
+]
---
+++ personagabrielfail/models/__init__.py
@@ -0,0 +1,4 @@
+"""All Models Personagabrielfail"""
+
+from .students import Students
+from ._8u11ects import _8u11ects
